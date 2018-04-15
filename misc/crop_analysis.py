fa1184c6bcf9a7865a610d0595f6771da6d2c0a6
Hassan <114499092@umail.ucc.ie>
2018-04-16 00:14:43 +0100

    working on report + added crop analysis

---
+++ misc/crop_analysis.py
@@ -0,0 +1,66 @@
+from os import listdir
+from os.path import isfile, join
+from skimage import io
+from tqdm import tqdm
+import numpy as np
+from tools.config import root_dir, TRAIN_DIR
+
+DIR = root_dir + TRAIN_DIR
+files = [f for f in listdir(DIR) if isfile(join(DIR, f))]
+
+
+def load_image(name):
+    return io.imread(join(DIR, name))
+
+
+def convert_to_bw(image):
+    return np.sum(image / np.shape(image)[0], axis=2) / 3
+
+
+def calc_crop_amount(image_file, brightness_limit=0.1):
+    img = load_image(image_file)
+
+    img = np.sum(img / 255, axis=2) / 3
+
+    brightness_of_areas = np.zeros(212)
+
+    for i in range(212):
+        brightness_of_areas[i] = np.sum(img) / (np.shape(img)[0] - (i * 2)) ** 2
+        img[i] = 0
+        img[len(img) - 1 - i] = 0
+        img[i + 1:-i - 1, i] = 0
+        img[i + 1:-i - 1, len(img) - 1 - i] = 0
+
+    for i in range(len(img)):
+        if brightness_of_areas[i] > brightness_limit:
+            return i
+
+
+def approx_total_crop_amount():
+    crop_approx = []
+
+    with tqdm(total=len(files)) as pbar:
+        for _file in files:
+            crop_approx.append(calc_crop_amount(_file))
+            pbar.update(1)
+
+    minimum_crop_approx_image = min(crop_approx)
+    file_with_min_crop = files[crop_approx.index(minimum_crop_approx_image)]
+    print("Minimum: ", minimum_crop_approx_image, " - ", file_with_min_crop)
+    print("")
+
+    maximum_crop_approx_image = max(crop_approx)
+    file_with_max_crop = files[crop_approx.index(maximum_crop_approx_image)]
+    print("Maximum: ", maximum_crop_approx_image, " - ", file_with_max_crop)
+    print("")
+
+    standard_deviation = np.std(np.array(crop_approx))
+    print("Standard Deviation: ", standard_deviation)
+    print("")
+
+    average_crop_amount = sum(crop_approx) / len(crop_approx)
+    print("Average Crop Amount: ", average_crop_amount)
+
+
+if __name__ == '__main__':
+    approx_total_crop_amount()
